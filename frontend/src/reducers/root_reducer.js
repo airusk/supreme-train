81bd943adbaa60f1e4534f2777aba029f609ef5f
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-11-17 20:16:47 -0800

    tweets front end working

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import errors_reducer from './errors_reducer';
 import session from './session_reducer';
+import tweets_reducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
   session,
-  errors: errors_reducer
+  errors: errors_reducer,
+  tweets: tweets_reducer
 });
 
 export default RootReducer;
